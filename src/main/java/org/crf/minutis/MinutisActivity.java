4ba3f73bc049587f526e3e67c9c5b735b90901cc
Xavier-Emmanuel VINCENT <xe.vincent@gmail.com>
2016-05-29 15:57:57 +0200

    Remove useless import

--- src/main/java/org/crf/minutis/MinutisActivity.java
+++ src/main/java/org/crf/minutis/MinutisActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.graphics.drawable.Drawable;
-import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
