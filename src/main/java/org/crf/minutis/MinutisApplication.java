d7879dab272eeec9f0d596af635d42cdfeb56a98
Xavier-Emmanuel VINCENT <xe.vincent@gmail.com>
2016-06-10 15:17:16 +0200

    Show a Dialog instead of a Toast for Acra

--- src/main/java/org/crf/minutis/MinutisApplication.java
+++ src/main/java/org/crf/minutis/MinutisApplication.java
@@ -6,15 +6,19 @@ import android.content.Context;
 import org.acra.*;
 import org.acra.annotation.*;
 
-@ReportsCrashes(mailTo = "",
-                mode = ReportingInteractionMode.TOAST,
-                resToastText = R.string.crash_toast_text)
+@ReportsCrashes(mailTo = BuildConfig.ACRA_EMAIL,
+                mode = ReportingInteractionMode.DIALOG,
+                resDialogText = R.string.crash_dialog_text,
+                resDialogPositiveButtonText = R.string.all_send,
+                resDialogTheme = R.style.AcraTheme_Dialog)
 public class MinutisApplication extends Application {
 	@Override
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
 
 		// The following line triggers the initialization of ACRA
-		ACRA.init(this);
+		if (!BuildConfig.ACRA_EMAIL.isEmpty()) {
+			ACRA.init(this);
+		}
 	}
 }
